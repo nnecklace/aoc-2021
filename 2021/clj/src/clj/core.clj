52c78dcd501bfe2c0a1cde918043e984fbba4b57
Niclas Forsman <niclas.forsman@futurice.com>
2021-12-01 19:09:34 +0200

    day 1

---
+++ 2021/clj/src/clj/core.clj
@@ -0,0 +1,16 @@
+(ns clj.core
+  (:gen-class)
+  (:require [clj.solutions.day1 :refer [solve]]))
+
+(def input (atom ""))
+
+(defn read-input
+  []
+  (doseq [ln (line-seq (java.io.BufferedReader. *in*))]
+    (swap! input str (str ln "\n")))
+  @input)
+
+(defn -main
+  [& args]
+  (let [input (read-input)]
+    (solve input)))
---
+++ 2021/clj/src/clj/solutions/day1.clj
@@ -0,0 +1,24 @@
+(ns clj.solutions.day1
+  (:require [clojure.string :as str]))
+
+(defn- task-1
+  [numbers]
+  (->> numbers
+       (partition 2 1)
+       (map #(apply < %))
+       (filter true?)
+       count))
+
+(defn task-2
+  [numbers]
+  (->> numbers
+       (partition 3 1)
+       (map #(reduce + %))
+       (partition 2 1)
+       (filter #(apply < %))
+       count))
+
+(defn solve
+  [input]
+  (let [numbers (as-> input $ (str/split $ #"\n") (map #(Integer/parseInt %) $))]
+    (println (task-2 numbers))))
